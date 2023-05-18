4e34afac79beef3e3cb2686920568d2866b23a99
wdAres <puneetsri9990@gmail.com>
2023-05-18 10:43:03 +0530

    Leave Management Page change

--- src/Components/AllModals/AddLeaveModal.jsx
+++ src/Components/AllModals/AddLeaveModal.jsx
@@ -27,13 +27,17 @@ const AddLeaveModal = (props) => {
                 <div onClick={closeHandler}><img src={Close} alt="" /></div>
             </div>
             <div className={classes.modal_data}>
-                <div className={classes.modal_data_div}>Name <span>{props.Obj.name}</span></div>
-                <div className={classes.modal_data_div}>Floor <span>{props.Obj.floor}</span></div>
+                <div className={classes.modal_data_div}>Name <span>{props.Obj.employee_name}</span></div>
+                <div className={classes.modal_data_div}>Floor <span>{props.Obj.floor_name}</span></div>
                 <div className={classes.modal_data_div}>Floor Incharge<span>Anil D Mishra</span></div>
                 <div className={classes.modal_data_div}>Loan <span><input type="date" /></span></div>
-                <div className={classes.modal_data_div}>Approval By Head<span><input type="text" /></span></div>
+                <div className={classes.modal_data_div}>Approval By Head<span>
+                <select className={classes.modal_select} >
+                    <option value="yes">Yes</option>
+                    <option value="no">No</option>
+                </select></span></div>
                 <div className={classes.modal_data_div}>Attach File<span><InpFile /></span></div>
-                <div className={classes.modal_data_div}>Recall Head<span><input type="checkbox"  /></span></div>
+                <div className={classes.modal_data_div}>Recall Head<span><input type="checkbox" /></span></div>
             </div>
             <div className={classes.modal_btn_container}>
                 <button className={classes.modal_btn1} onClick={closeHandler}>Cancel</button>
