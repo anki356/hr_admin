0e92cedbccf2e01e8831f2aa61242afaee7739c7
anki356 <anki356@gmail.com>
2023-05-18 10:03:53 +0530

    Leave Approval

--- src/App.jsx
+++ src/App.jsx
@@ -92,7 +92,7 @@ const App = () => {
               <Route exact path="/fine_approvals" element={<FineApprovals />} />
 
               <Route exact path="/leave_management" element={<LeaveManagement />} />
-              <Route exact path="/leave_approvals" element={<LeaveApprovals />} />
+              <Route exact path="/leave_approvals/:id/:employee_id" element={<LeaveApprovals />} />
 
               <Route exact path="/loan_emi" element={<LoanEmi />} />
               <Route exact path="/loan_approvals" element={<LoanApprovals />} />
--- src/Pages/LeaveManagement/LeaveApprovals/LeaveApprovals.jsx
+++ src/Pages/LeaveManagement/LeaveApprovals/LeaveApprovals.jsx
@@ -1,81 +1,179 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
 import BottomButtonContainer from '../../../Components/BottomButtonContainer/BottomButtonContainer'
 import Heading from '../../../Components/Heading/Heading'
 import LabeledInput from '../../../Components/LabeledInput/LabeledInput'
 import MainTable from '../../../Components/MainTable/MainTable'
 import AdditionalInfoContainer from '../../../UI/AdditionalInfoContainer/AdditionalInfoContainer'
 import DetailsDivContainer from '../../../UI/DetailsDivContainers/DetailsDivContainer'
-import data from './data'
 import classes from './LeaveApprovals.module.css'
+import { useNavigate, useParams } from 'react-router-dom';
+import useHttp from '../../../Hooks/use-http'
+import Cookies from 'universal-cookie'
+import moment from 'moment'
 
+import axios from 'axios'
 const LeaveApprovals = () => {
-
-  const employee_data = [{
-    "title": "Electrician",
-    "value": "Royall"
-  }, {
-    "title": "Construction Manager",
-    "value": "Sayer"
-  }, {
-    "title": "Electrician",
-    "value": "Aliza"
-  }, {
-    "title": "Engineer",
-    "value": "Jemie"
-  }, {
-    "title": "Subcontractor",
-    "value": "Jacklin"
-  }, {
-    "title": "Subcontractor",
-    "value": "Garold"
-  }, {
-    "title": "Engineer",
-    "value": "Dorry"
-  }, {
-    "title": "Construction Expeditor",
-    "value": "Matias"
-  }, {
-    "title": "Subcontractor",
-    "value": "Genevieve"
-  }, {
-    "title": "Construction Foreman",
-    "value": "Catlin"
-  }]
-
-  const leave_info = [
-    {
-      title: 'Date',
-      value: '31 march To 3 April 2023'
-    },
-    {
-      title: 'Days',
-      value: '4 Days'
-    },
-    {
-      title: 'Recall Head',
-      value: 'Yes'
-    },
-    {
-      title: 'Head Approval',
-      value: 'Yes'
+  const url="http://localhost:9000/"
+  const cookies = new Cookies();
+  const navigate=useNavigate()
+  const token = cookies.get('token')
+  const [data,setData]=useState([])
+  const { sendRequest: fetchEmployeeDetails } = useHttp()
+  const { sendRequest: fetchLeave } = useHttp()
+  const {id,employee_id}=useParams()
+  const [div_data,setDivData]=useState([])
+  const [leave_info,setLeaveInfo]=useState(null)
+  const[from_date,setFromDate]=useState(null)
+  const[to_date,setToDate]=useState(null)
+  const [reason,setReason]=useState(null)
+  // const employee_data = [{
+  //   "title": "Electrician",
+  //   "value": "Royall"
+  // }, {
+  //   "title": "Construction Manager",
+  //   "value": "Sayer"
+  // }, {
+  //   "title": "Electrician",
+  //   "value": "Aliza"
+  // }, {
+  //   "title": "Engineer",
+  //   "value": "Jemie"
+  // }, {
+  //   "title": "Subcontractor",
+  //   "value": "Jacklin"
+  // }, {
+  //   "title": "Subcontractor",
+  //   "value": "Garold"
+  // }, {
+  //   "title": "Engineer",
+  //   "value": "Dorry"
+  // }, {
+  //   "title": "Construction Expeditor",
+  //   "value": "Matias"
+  // }, {
+  //   "title": "Subcontractor",
+  //   "value": "Genevieve"
+  // }, {
+  //   "title": "Construction Foreman",
+  //   "value": "Catlin"
+  // }]
+  useEffect(()=>{
+    const listEmployeeDetails= (employeeDetails) => {
+      setDivData([{
+        title:"Name",
+        value:employeeDetails.employeesResult[0].name
+      },{
+  title:'SuperVisor Name',
+  value:employeeDetails.headEmployeesResult[0]?.head_employee_name
+      },{
+        title:'Designation',
+  value:employeeDetails.employeesResult[0].role_name
+      },{
+        title:'Floor Name',
+  value:employeeDetails.employeesResult[0].floor_name
+  
+        }, {
+          title: 'Gender',
+          value: employeeDetails.employeesResult[0].gender
+  
+        }, {
+          title: 'Store name',
+          value: employeeDetails.employeesResult[0].store_name
+        }, {
+          title: 'Store Department',
+          value: employeeDetails.employeesResult[0].store_department_name
+        }])
     }
-  ]
+    fetchEmployeeDetails({ url: url+"api/getEmployeeDetails?id="+employee_id }, listEmployeeDetails)
+    const listLeave= (leaveDetails) => {
+      setFromDate(leaveDetails[0].from_date)
+      setToDate(leaveDetails[0].to_date)
+      let from_date=leaveDetails[0].from_date
+      let to_date=leaveDetails[0].to_date
+     let from_day=moment(from_date).date()
+     let from_month=moment(from_date).month()
+     let year=moment(from_date).year()
+     let to_day=moment(to_date).date()
+     let to_month=moment(to_date).month()
+     let from=moment(from_date)
+     let to=moment(to_date).add(1,'d')
+     let monthArray=["January, February","March","April","May","June","July","August","September","October","November","December"]
+setLeaveInfo([
+  {
+    title: 'Date',
+    value: monthArray[from_month-1]+" "+ from_day+" "+  " to "+monthArray[to_month-1]+" "+ to_day+" "+" ,"+year
+  },
+  {
+    title: 'Days',
+    value: to.diff(from ,'d')
+  },
+  {
+    title: 'Recall Head',
+    value: leaveDetails[0].recall_head===1?'Yes':'No'
+  },
+  {
+    title: 'Head Approval',
+    value: leaveDetails[0].head_approval===1?'Yes':'No'
+  }
+])
+setData([{
+  document:leaveDetails[0].document
+}])
+    }
+    fetchLeave({ url: url+"api/getLeave?id="+id }, listLeave)
+  },[])
+  
+  console.log(data)
+  
 
   const tableHeading=[{heading:'Documents'}]
   const tableKeys = ['document']
+  function approve(){
+    const headers={"Authorization":"Bearer "+token}
+    axios.patch(url+"api/updateLeaveStatus",{
+      
+        "status":"Approved",
+        "from_date":from_date.split("T")[0],
+        "to_date":to_date.split("T")[0],
+        "employee_id":employee_id
+    
+    },{headers}).then((response)=>{
+if(response){
+    navigate(-1) 
+}
+    })
+   
+}
+function cancel(){
+  const headers={"Authorization":"Bearer "+token}
+  axios.patch(url+"api/updateLeaveStatus",{
+    
+      "status":"Rejected",
+      "from_date":from_date.split("T")[0],
+      "to_date":to_date.split("T")[0],
+      "employee_id":employee_id,
+      "reason":reason
+  
+  },{headers}).then((response)=>{
+if(response){
+  navigate(-1) 
+}
+  })
+ 
 
+}
   return (
     <React.Fragment>
       <Heading heading={'Leave Approvals'} />
-      <DetailsDivContainer data={employee_data} />
+      <DetailsDivContainer data={div_data} />
       <div className='uni_container'>
         <h3 className='uni_heading'>Leave Information</h3>
         <AdditionalInfoContainer data={leave_info} />
-        <LabeledInput cls={true} id={'val'} title={'Reason If Rejected'} img={false} />
+        <LabeledInput cls={true} id={'val'} title={'Reason If Rejected'} img={false} func2={setReason} />
       </div>
       <h3 className='uni_heading'>Attached File</h3>
       <MainTable headings={tableHeading} keys={tableKeys} data={data} height={true} />
-      <BottomButtonContainer cancel={'Back'} approve={'Continue'} />
+      <BottomButtonContainer cancel={'Reject'} approve={'Approve'} func={true} cancelRequests={cancel} func2={approve} />
     </React.Fragment>
   )
 }
