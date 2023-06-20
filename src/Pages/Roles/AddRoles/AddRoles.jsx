1c2c628876477a3a6825318171b07897661e31e8
wdAres <puneetsri9990@gmail.com>
2023-06-20 10:38:45 +0530

    update

--- src/Pages/Roles/AddRoles/AddRoles.jsx
+++ src/Pages/Roles/AddRoles/AddRoles.jsx
@@ -7,119 +7,128 @@ import axios from 'axios'
 import Cookies from 'universal-cookie'
 
 import { useNavigate, useParams } from 'react-router-dom'
+import SwitchButton from '../../../Components/SwitchButton/SwitchButton'
 const AddRoles = () => {
-const[role_name,setRoleName]=useState(null)
-const[floor_name,setFloorName]=useState(null)
-const[store_name,setStoreName]=useState(null)
-const[password,setPassword]=useState(null)
-const[username,setUsername]=useState(null)
-const [floorOptions,setFloorOptions]=useState([])
-const [storesOptions,setStoreOptions]=useState([])
-const url = "http://localhost:9000/"
-const cookies = new Cookies();
-const token = cookies.get('token')
-const navigate = useNavigate()
-useEffect(()=>{
-    const headers = { "Authorization": "Bearer " + token }
-axios.get(url+"api/getFloors",{headers}).then((response)=>{
-setFloorOptions(response.data)
-})
-axios.get(url+"api/getStores",{headers}).then((response)=>{
-    setStoreOptions(response.data)
-    })
-},[])
+    const [role_name, setRoleName] = useState(null)
+    const [floor_name, setFloorName] = useState(null)
+    const [store_name, setStoreName] = useState(null)
+    const [password, setPassword] = useState(null)
+    const [username, setUsername] = useState(null)
+    const [floorOptions, setFloorOptions] = useState([])
+    const [storesOptions, setStoreOptions] = useState([])
+    const url = "http://localhost:9000/"
+    const cookies = new Cookies();
+    const token = cookies.get('token')
+    const navigate = useNavigate()
+    useEffect(() => {
+        const headers = { "Authorization": "Bearer " + token }
+        axios.get(url + "api/getFloors", { headers }).then((response) => {
+            setFloorOptions(response.data)
+        })
+        axios.get(url + "api/getStores", { headers }).then((response) => {
+            setStoreOptions(response.data)
+        })
+    }, [])
     const inputs = [
         {
             title: 'Role',
             id: 'role',
             ph: '',
-            value:role_name,
-            func:setRoleName,
-            required:true
+            value: role_name,
+            func: setRoleName,
+            required: true
         },
         {
             title: 'Password',
             id: 'password',
             ph: '',
-            value:password,
-            func:setPassword,
-            required:false
+            value: password,
+            func: setPassword,
+            required: false
         },
         {
             title: 'Username',
             id: 'username',
             ph: '',
-            value:username,
-            func:setUsername,
-            required:false
+            value: username,
+            func: setUsername,
+            required: false
         },
-       
+
     ]
-    function add(e){
+    function add(e) {
         const headers = { "Authorization": "Bearer " + token }
         e.preventDefault()
-axios.post(url+"api/addRole",{
-role_name:role_name,
-floor_id:floor_name,
-store_id:store_name,
-},{headers}).then((response)=>{
-    if(response){
-        if(username!==undefined&&username!==null&&username!==''&&password!==undefined&&password!==null&&password!==''){
+        axios.post(url + "api/addRole", {
+            role_name: role_name,
+            floor_id: floor_name,
+            store_id: store_name,
+        }, { headers }).then((response) => {
+            if (response) {
+                if (username !== undefined && username !== null && username !== '' && password !== undefined && password !== null && password !== '') {
 
-            axios.post(url+"api/auth/register",{
-                role:role_name,
-                username:username,
-                password:password
-            },{headers}).then((responseOne)=>{
-                if(responseOne){
-                    navigate(-1)
+                    axios.post(url + "api/auth/register", {
+                        role: role_name,
+                        username: username,
+                        password: password
+                    }, { headers }).then((responseOne) => {
+                        if (responseOne) {
+                            navigate(-1)
+                        }
+                    })
                 }
-            })
-        }
-    }
-})
+            }
+        })
     }
     console.log(store_name)
     console.log(floor_name)
-    function cancel(){
-navigate(-1)
+    function cancel() {
+        navigate(-1)
     }
-  return (
-    <React.Fragment>
-        <Heading heading='Add Role' />
-        <form onSubmit={add}>
-        <div className={classes.container}>
-            {inputs.map((element,index)=>(
-                <LabeledInput required={element.required} title={element.title} value={element.value} key={index} ph={element.ph} id={element.id} cls={true} img={false} func2={(data)=>element.func(data)}/>
-            ))}
-            <div className={classes.store_div}>
-                    <label htmlFor='floor'>Floor</label>
-                    <select name="floor"  onChange={(e)=>setFloorName(e.target.value)} id="floor">
-                    {floorOptions.map((val, index) => (
-                        <option key={index} selected={floor_name===val.id} value={val.id}>{val.name}</option>
+    return (
+        <React.Fragment>
+            <Heading heading='Add Role' />
+            <form onSubmit={add}>
+                <div className={classes.container}>
+                    {inputs.map((element, index) => (
+                        <LabeledInput required={element.required} title={element.title} value={element.value} key={index} ph={element.ph} id={element.id} cls={true} img={false} func2={(data) => element.func(data)} />
                     ))}
-                       
-                    </select>
+                    <div className={classes.store_div}>
+                        <label htmlFor='floor'>Floor</label>
+                        <select name="floor" onChange={(e) => setFloorName(e.target.value)} id="floor">
+                            {floorOptions.map((val, index) => (
+                                <option key={index} selected={floor_name === val.id} value={val.id}>{val.name}</option>
+                            ))}
+
+                        </select>
+                    </div>
+                    <div className={classes.store_div}>
+                        <label htmlFor='Store'>Store</label>
+                        <select name="Store" onChange={(e) => setStoreName(e.target.value)} id="Store">
+                            {storesOptions.map((val, index) => (
+                                <option key={index} selected={store_name === val.id} value={val.id}>{val.name}</option>
+                            ))}
+
+                        </select>
+                    </div>
                 </div>
-                <div className={classes.store_div}>
-                    <label htmlFor='Store'>Store</label>
-                    <select name="Store"  onChange={(e)=>setStoreName(e.target.value)} id="Store">
-                    {storesOptions.map((val, index) => (
-                        <option key={index} selected={store_name===val.id} value={val.id}>{val.name}</option>
-                    ))}
-                       
-                    </select>
+                <div className={classes.switch_container}>
+                    <h2>Permissions</h2>
+                    <SwitchButton label={'Attendance'} />
+                    <SwitchButton label={'Timming Approval'} />
+                    <SwitchButton label={'Interview'} />
+                    <SwitchButton label={'Employee Detail'} />
+                    <SwitchButton label={'Salary'} />
+                    <SwitchButton label={'Fine Management'} />
                 </div>
-        </div>
-        <div className={classes.btn_container}>
-      
-      <button  className={classes.cancel} onClick={(event)=> cancel(event)  }>Cancel</button>
-      <button type={'submit'}  className={classes.accept} >Add Role</button>
-    </div>
-        </form>
-       
-    </React.Fragment>
-  )
+                <div className={classes.btn_container}>
+                    <button className={classes.cancel} onClick={(event) => cancel(event)}>Cancel</button>
+                    <button type={'submit'} className={classes.accept} >Add Role</button>
+                </div>
+            </form>
+
+        </React.Fragment>
+    )
 }
 
 export default AddRoles
--- src/Pages/Roles/EditRole/EditRoles.jsx
+++ src/Pages/Roles/EditRole/EditRoles.jsx
@@ -7,111 +7,121 @@ import axios from 'axios'
 import Cookies from 'universal-cookie'
 import { useNavigate, useParams } from 'react-router-dom'
 const EditRoles = () => {
-const[role_name,setRoleName]=useState(null)
-const[floor_name,setFloorName]=useState(null)
-const[store_name,setStoreName]=useState(null)
-const[password,setPassword]=useState(null)
-const[username,setUsername]=useState(null)
-const [floorOptions,setFloorOptions]=useState([])
-const [storesOptions,setStoreOptions]=useState([])
-const url = "http://localhost:9000/"
-const cookies = new Cookies();
-const token = cookies.get('token')
-const navigate = useNavigate()
-const {id}=useParams()
-useEffect(()=>{
-    const headers = { "Authorization": "Bearer " + token }
-    axios.get(url+"api/getRoleData?id="+id,{headers}).then((response)=>{
-        setFloorName(response.data[0].floor_name)
-        setStoreName(response.data[0].store_name)
-        setUsername(response.data[0].username)
-        setRoleName(response.data[0].role_name)
-        }) 
-axios.get(url+"api/getFloors",{headers}).then((response)=>{
-setFloorOptions(response.data)
-})
-axios.get(url+"api/getStores",{headers}).then((response)=>{
-    setStoreOptions(response.data)
-    })
-},[])
+    const [role_name, setRoleName] = useState(null)
+    const [floor_name, setFloorName] = useState(null)
+    const [store_name, setStoreName] = useState(null)
+    const [password, setPassword] = useState(null)
+    const [username, setUsername] = useState(null)
+    const [floorOptions, setFloorOptions] = useState([])
+    const [storesOptions, setStoreOptions] = useState([])
+    const url = "http://localhost:9000/"
+    const cookies = new Cookies();
+    const token = cookies.get('token')
+    const navigate = useNavigate()
+    const { id } = useParams()
+    useEffect(() => {
+        const headers = { "Authorization": "Bearer " + token }
+        axios.get(url + "api/getRoleData?id=" + id, { headers }).then((response) => {
+            setFloorName(response.data[0].floor_name)
+            setStoreName(response.data[0].store_name)
+            setUsername(response.data[0].username)
+            setRoleName(response.data[0].role_name)
+        })
+        axios.get(url + "api/getFloors", { headers }).then((response) => {
+            setFloorOptions(response.data)
+        })
+        axios.get(url + "api/getStores", { headers }).then((response) => {
+            setStoreOptions(response.data)
+        })
+    }, [])
     const inputs = [
         {
             title: 'Role',
             id: 'role',
             ph: '',
-            value:role_name,
-            func:setRoleName,
-            required:true
+            value: role_name,
+            func: setRoleName,
+            required: true
         },
         {
             title: 'Password',
             id: 'password',
             ph: '',
-            value:password,
-            func:setPassword,
-            required:false
+            value: password,
+            func: setPassword,
+            required: false
         },
         {
             title: 'Username',
             id: 'username',
             ph: '',
-            value:username,
-            func:setUsername,
-            required:false
+            value: username,
+            func: setUsername,
+            required: false
         },
     ]
-    function edit(){
+    function edit() {
         const headers = { "Authorization": "Bearer " + token }
-axios.patch(url+"api/editRole/"+id,{
-role_name:role_name,
-floor_id:floor_name,
-store_id:store_name,
-username:username,
-password:password
-},{headers}).then((response)=>{
-    if(response){
-   navigate(-1)
+        axios.patch(url + "api/editRole/" + id, {
+            role_name: role_name,
+            floor_id: floor_name,
+            store_id: store_name,
+            username: username,
+            password: password
+        }, { headers }).then((response) => {
+            if (response) {
+                navigate(-1)
+            }
+        })
     }
-})}
     console.log(store_name)
     console.log(floor_name)
-    function cancel(){
-navigate(-1)
+    function cancel() {
+        navigate(-1)
     }
     return (
         <React.Fragment>
             <Heading heading='Edit Roles' />
             <form onSubmit={edit}>
-            <div className={classes.container}>
-                {inputs.map((element, index) => (
-                    <LabeledInput required={element.required} title={element.title} value={element.value} key={index} ph={element.ph} id={element.id} cls={true} img={false} func2={(data)=>element.func(data)}/>
-                ))}
-               <div className={classes.store_div}>
-                    <label htmlFor='floor'>Floor</label>
-                    <select name="floor"  onChange={(e)=>setFloorName(e.target.value)} id="floor">
-                    {floorOptions.map((val, index) => (
-                        <option key={index} selected={floor_name===val.id} value={val.id}>{val.name}</option>
+                <div className={classes.container}>
+                    {inputs.map((element, index) => (
+                        <LabeledInput required={element.required} title={element.title} value={element.value} key={index} ph={element.ph} id={element.id} cls={true} img={false} func2={(data) => element.func(data)} />
                     ))}
-                       
-                    </select>
+                    <div className={classes.store_div}>
+                        <label htmlFor='floor'>Floor</label>
+                        <select name="floor" onChange={(e) => setFloorName(e.target.value)} id="floor">
+                            {floorOptions.map((val, index) => (
+                                <option key={index} selected={floor_name === val.id} value={val.id}>{val.name}</option>
+                            ))}
+
+                        </select>
+                    </div>
+                    <div className={classes.store_div}>
+                        <label htmlFor='Store'>Store</label>
+                        <select name="Store" onChange={(e) => setStoreName(e.target.value)} id="Store">
+                            {storesOptions.map((val, index) => (
+                                <option key={index} selected={store_name === val.id} value={val.id}>{val.name}</option>
+                            ))}
+
+                        </select>
+                    </div>
                 </div>
-                <div className={classes.store_div}>
-                    <label htmlFor='Store'>Store</label>
-                    <select name="Store"  onChange={(e)=>setStoreName(e.target.value)} id="Store">
-                    {storesOptions.map((val, index) => (
-                        <option key={index} selected={store_name===val.id} value={val.id}>{val.name}</option>
-                    ))}
-                       
-                    </select>
+                <div className={classes.switch_container}>
+                    <h2>Permissions</h2>
+                    <SwitchButton label={'Attendance'} />
+                    <SwitchButton label={'Timming Approval'} />
+                    <SwitchButton label={'Interview'} />
+                    <SwitchButton label={'Employee Detail'} />
+                    <SwitchButton label={'Salary'} />
+                    <SwitchButton label={'Fine Management'} />
                 </div>
-            </div>
-            <div className={classes.btn_container}>
-      
-      <button  className={classes.cancel} onClick={(event)=> cancel(event)  }>Cancel</button>
-      <button type={'submit'}  className={classes.accept} >Edit Role</button>
-    </div>
+                <div className={classes.btn_container}>
+
+                    <button className={classes.cancel} onClick={(event) => cancel(event)}>Cancel</button>
+                    <button type={'submit'} className={classes.accept} >Edit Role</button>
+                </div>
             </form>
-           
+
         </React.Fragment>
     )
 }
