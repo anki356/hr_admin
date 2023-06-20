62310685b8db7186ee59990ee616eb3c6aef885a
wdAres <puneetsri9990@gmail.com>
2023-06-20 14:01:34 +0530

    Update HierarchyTable.jsx

--- src/Pages/HierarchyPage/HierarchyTable.jsx
+++ src/Pages/HierarchyPage/HierarchyTable.jsx
@@ -1,4 +1,4 @@
-import React,{useEffect,useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import { Tree, TreeNode } from 'react-organizational-chart';
 import classes from './HierarchyPage.module.css'
 import user from '../../assets/User_fill.png'
@@ -8,43 +8,114 @@ import Cookies from 'universal-cookie'
 import axios from 'axios'
 const url = "http://localhost:9000/"
 const Card = (props) => {
-    
+
     return (
         <div className={classes.hierarchy_card}>
-            <img src={url+props?.data?.photo} alt="" />
+            <img src={url + props?.data?.photo} alt="" />
             <h4>{props?.data?.employee_name}</h4>
             <p>{props.role_name}</p>
             {/* <p>Location : location 1 4th Floor</p> */}
             <div className={classes.hc_no_of_child}>
-                3 <img src={user} alt="" />
+                {props.cl} <img src={user} alt="" />
             </div>
         </div>
     );
 }
 
 const HierarchyTable = () => {
-    const[super_admin,setSuperAdmin]=useState([])
-   const [admin,setAdmin]=useState([])
+    const [super_admin, setSuperAdmin] = useState([])
+    const [admin, setAdmin] = useState([])
+
+    // Locations
+    const [locations, setLocations] = useState([])
+    const [officedept, setOfficeDept] = useState([])
+    const [storeDept, setStoreDept] = useState([])
+    const [warehouseDept, setWarehouseDept] = useState([])
+
     const cookies = new Cookies();
     const token = cookies.get('token')
-    useEffect(()=>{
-        const headers={"Authorization":"Bearer "+token}
-axios.get(url+"api/getEmployeesBasedOnRole?role_name='Super Admin'",{headers}).then((response)=>{
-setSuperAdmin(response.data)
-})
-axios.get(url+"api/getEmployeesBasedOnRole?role_name='Admin'",{headers}).then((response)=>{
-    setAdmin(response.data)
-    })
-    },[])
-    
+
+    const getLocations = async () => {
+        const response = await fetch(`${url}api/getLocations`, {
+            method: "GET",
+            headers: {
+                "Authorization": "Bearer " + token
+            }
+        })
+
+        const result = await response.json();
+        console.log('our loc', result);
+        setLocations(result)
+    }
+    const updateLocation = (index) => {
+        switch (index) {
+            case 0:
+                return setOfficeDept(
+                    ['Account', 'Marketing', 'Maintainacne and Security', 'HR', 'Data Analytics', 'Customer Care', 'IT']
+                )
+
+            case 1:
+                return setStoreDept(
+                    ['Sales', 'Alteration', 'Goods Return', 'Secuity and Maintaince', 'Visiual Mercendises', 'Cashier']
+                )
+
+            case 2:
+                return setWarehouseDept(
+                    ['Dispatch', 'Goods Return', 'Data Inventory', 'Mercendising', 'Secuity and Maintaince']
+                )
+
+            default:
+                break;
+        }
+
+    }
+
+    // useEffect(() => {
+    //     const headers = { "Authorization": "Bearer " + token }
+    //     axios.get(url + "api/getEmployeesBasedOnRole?role_name='Super Admin'", { headers }).then((response) => {
+    //         setSuperAdmin(response.data)
+    //     })
+    //     axios.get(url + "api/getEmployeesBasedOnRole?role_name='Admin'", { headers }).then((response) => {
+    //         setAdmin(response.data)
+    //     })
+    // }, [])
+
+    useEffect(() => {
+        getLocations();
+
+    }, [])
+
     return (
         <div className={classes.hierarchy_table}>
-            <Tree label={<div className={classes.ht_nodes}><Card data={super_admin[0]} role_name="Super Admin" /> <span>Super Admin</span></div>}>
-               { admin.map((val)=>{
-                    return <TreeNode label={<div className={classes.ht_nodes}><Card data={val} role_name="Admin" /> <span>Admin</span></div>}></TreeNode>
+            <Tree label={<div className={classes.ht_nodes}>
+                <Card data={super_admin[0]} role_name="Super Admin" cl={locations.length} /> <span>Super Admin</span></div>}>
+                {locations && locations.map((val, index) => {
+                    return <TreeNode label={<div className={classes.ht_nodes} onClick={() => updateLocation(index)}>
+                        <Card data={val.name} role_name="locations" /> <span>{val.name}</span></div>}>
+                        {index == 0 ?
+                            officedept.map((element, index) => (
+                                <TreeNode label={<div className={classes.ht_nodes}>
+                                    <Card data={element} role_name='Office Depatment' /> <span>{element}</span></div>}>
+                                </TreeNode>
+                            )) : ''
+                        }
+                        {index == 1 ?
+                            storeDept.map((element, index) => (
+                                <TreeNode label={<div className={classes.ht_nodes}>
+                                    <Card data={element} role_name='Store Department' /> <span>{element}</span></div>}>
+                                </TreeNode>
+                            )) : ''
+                        }
+                        {index == 2 ?
+                            warehouseDept.map((element, index) => (
+                                <TreeNode label={<div className={classes.ht_nodes}>
+                                    <Card data={element} role_name='Warehouse Department' /> <span>{element}</span></div>}>
+                                </TreeNode>
+                            )) : ''
+                        }
+                    </TreeNode>
                 })}
-                
-                
+
             </Tree>
 
         </div>
